cef5639b39129832c1d6632dec5fc6ac11ab4e8b
Cobonkoi <immortals11@hotmail.co.uk>
2019-09-10 21:04:42 +0000

    Added ability to search via region, platform and username. Output currently shown on console

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ function printDataToConsole(data) {
 }
 
 getData(printDataToConsole);
-*/
+
 
 const baseURL = "https://ow-api.com/v1/stats/pc/eu/MLGesus-21679/profile";
 
@@ -30,6 +30,38 @@ function getData(type, cb) {
     };
 
     xhr.open("GET", baseURL);
+    xhr.send();
+}
+
+function writeToDocument(type) {
+    getData(type, function(data) {
+        console.dir(data);
+        document.getElementById("stat-games").innerHTML = `Played ${data.competitiveStats.games.played} / Won ${data.competitiveStats.games.played}`;
+    });
+} */
+
+/*Usernames for tests
+Zael-2375
+Sharpy-2695
+MLGesus-21679
+*/
+
+const baseURL = "https://ow-api.com/v1/stats/";
+
+function getData(type, cb) {
+    var xhr = new XMLHttpRequest();
+    
+    var platform = document.getElementById("form-platform").value;
+    var region = document.getElementById("form-region").value;
+    var username = document.getElementById("form-username").value;
+    
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            cb(JSON.parse(this.responseText));
+        }
+    };
+
+    xhr.open("GET", baseURL + platform + "/" + region + "/" + username + "/profile");
     xhr.send();
 }
 
