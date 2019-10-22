c2062bc90d7501404a05727e162ed30440887dbc
Cobonkoi <immortals11@hotmail.co.uk>
2019-10-22 10:02:08 +0000

    Added loading screen to search page while data loads. Error will be shown when a profile is private on search page

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,31 +1,37 @@
 const baseURL = "https://ow-api.com/v1/stats/";
 
 const bestArray = ['allDamageDoneMostInGame', 'barrierDamageDoneMostInGame', 'defensiveAssistsMostInGame', 'eliminationsMostInGame', 'finalBlowsMostInGame', 'healingDoneMostInGame',
-                    'heroDamageDoneMostInGame', 'killsStreakBest', 'meleeFinalBlowsMostInGame', 'multikillsBest', 'objectiveKillsMostInGame', 'objectiveTimeMostInGame',
-                    'offensiveAssistsMostInGame', 'soloKillsMostInGame', 'timeSpentOnFireMostInGame', 'turretsDestroyedMostInGame'];
+    'heroDamageDoneMostInGame', 'killsStreakBest', 'meleeFinalBlowsMostInGame', 'multikillsBest', 'objectiveKillsMostInGame', 'objectiveTimeMostInGame',
+    'offensiveAssistsMostInGame', 'soloKillsMostInGame', 'timeSpentOnFireMostInGame', 'turretsDestroyedMostInGame'
+];
 const combatArray = ['barrierDamageDone', 'damageDone', 'deaths', 'eliminations', 'finalBlows', 'heroDamageDone', 'meleeFinalBlows', 'multikills', 'objectiveKills',
-                            'objectiveTime', 'soloKills', 'timeSpentOnFire'];
+    'objectiveTime', 'soloKills', 'timeSpentOnFire'
+];
 const assistsArray = ['defensiveAssists', 'healingDone', 'offensiveAssists'];
 const averageArray = ['allDamageDoneAvgPer10Min', 'barrierDamageDoneAvgPer10Min', 'deathsAvgPer10Min', 'eliminationsAvgPer10Min', 'finalBlowsAvgPer10Min', 'healingDoneAvgPer10Min',
-                            'heroDamageDoneAvgPer10Min', 'objectiveKillsAvgPer10Min', 'objectiveTimeAvgPer10Min', 'soloKillsAvgPer10Min', 'timeSpentOnFireAvgPer10Min'];
+    'heroDamageDoneAvgPer10Min', 'objectiveKillsAvgPer10Min', 'objectiveTimeAvgPer10Min', 'soloKillsAvgPer10Min', 'timeSpentOnFireAvgPer10Min'
+];
 const awardsArray = ['cards', 'medals', 'medalsBronze', 'medalsGold', 'medalsSilver'];
 const gameArray = ['gamesWon', 'timePlayed'];
 const miscellaneousArray = ['teleporterPadsDestroyed', 'turretsDestroyed'];
 
+let dataDownload = {};
 
-function getData(cb) {
+
+function getData(platformInput, regionInput, usernameInput, cb) {
     var xhr = new XMLHttpRequest();
-    
-    var platform = document.getElementById("searchFormPlatform").value;
-    var region = document.getElementById("searchFormRegion").value;
-    var username = document.getElementById("searchFormUsername").value;
-    
+
+    var platform = document.getElementById(platformInput).value;
+    var region = document.getElementById(regionInput).value;
+    var username = document.getElementById(usernameInput).value;
+
     if (platform == "pc") {
         var profileUrl = baseURL + platform + "/" + region + "/" + username + "/complete";
-    } else {
-        var profileUrl = baseURL + platform + "/"  + username + "/complete";
     }
-    
+    else {
+        var profileUrl = baseURL + platform + "/" + username + "/complete";
+    }
+
     xhr.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             cb(JSON.parse(this.responseText));
@@ -36,65 +42,190 @@ function getData(cb) {
     xhr.send();
 }
 
-function writeToDocument() {
-    getData(function(data) {
-        
+function writeToDocument(platformInput, regionInput, usernameInput) {
+    document.getElementById("errorMessage").innerHTML = "";
+    document.getElementById("loaderBackground").classList.add("loader-background");
+    document.getElementById("loader").classList.add("loader");
+
+    getData(platformInput, regionInput, usernameInput, function(data) {
+        dataDownload = data;
+
+        console.dir(dataDownload);
+        if (dataDownload['private'] === true) {
+            document.getElementById("errorMessage").innerHTML = "Profile is Private - Please set to public in Overwatch > Social settings";
+        }
+        else {
+            changeDocumentData();
+        }
+        document.getElementById("loaderBackground").classList.remove('loader-background');
+        document.getElementById("loader").classList.remove('loader');
+    });
+}
+
+function changeDocumentData() {
+    if (Object.entries(dataDownload).length !== 0) {
+
         var gameType = document.getElementById("gameTypeSelect").value;
         var heroName = document.getElementById("heroNameSelect").value;
-        
-        console.dir(data);
-        
+
         //Profile Text
-        document.getElementById("profileUsername").innerHTML = data['name'];
-        document.getElementById("profileLevel").innerHTML = data['level'];
-        document.getElementById("profileEndoresmentLevel").innerHTML = data['endorsement'];
+        document.getElementById("profileUsername").innerHTML = dataDownload['name'];
+        document.getElementById("profileLevel").innerHTML = dataDownload['level'] + (dataDownload['prestige'] * 100);
+        document.getElementById("profileEndoresmentLevel").innerHTML = dataDownload['endorsement'];
         document.getElementById("profilePlatform").innerHTML = document.getElementById("searchFormPlatform").value;
         document.getElementById("profileRegion").innerHTML = " - " + document.getElementById("searchFormRegion").value;
-        
-        
+
+
         //Images
-        document.getElementById("profilePictureImage").src = data['icon'];
-        document.getElementById("profileLevelImage").src = data['levelIcon'];
-        document.getElementById("profileEndoresmentImage").src = data['endorsementIcon'];
-        document.getElementById("profilePrestigeImage").src = data['prestigeIcon'];
-        
+        document.getElementById("profilePictureImage").src = dataDownload['icon'];
+        /*document.getElementById("profileLevelImage").src = dataDownload['levelIcon'];
+        document.getElementById("profileEndoresmentImage").src = dataDownload['endorsementIcon'];
+        document.getElementById("profilePrestigeImage").src = dataDownload['prestigeIcon'];*/
+
         //Table Data
         bestArray.forEach(function(statistic) {
-            document.getElementById(statistic).innerHTML = data[gameType]['careerStats'][heroName]['best'][statistic];
+            if (dataDownload[gameType]['careerStats'][heroName] == null) {
+                document.getElementById(statistic).innerHTML = "N/A";
+            }
+            else {
+                if (dataDownload[gameType]['careerStats'][heroName]['best'] == null) {
+                    document.getElementById(statistic).innerHTML = "N/A";
+                }
+                else {
+                    if (dataDownload[gameType]['careerStats'][heroName]['best'][statistic] == null) {
+                        document.getElementById(statistic).innerHTML = "N/A";
+                    }
+                    else {
+                        document.getElementById(statistic).innerHTML = dataDownload[gameType]['careerStats'][heroName]['best'][statistic];
+                    }
+                }
+            }
         });
-        
-        combatArray.forEach(function(statistic2) {
-            document.getElementById(statistic2).innerHTML = data[gameType]['careerStats'][heroName]['combat'][statistic2];
+
+        combatArray.forEach(function(statistic) {
+            if (dataDownload[gameType]['careerStats'][heroName] == null) {
+                document.getElementById(statistic).innerHTML = "N/A";
+            }
+            else {
+                if (dataDownload[gameType]['careerStats'][heroName]['combat'] == null) {
+                    document.getElementById(statistic).innerHTML = "N/A";
+                }
+                else {
+                    if (dataDownload[gameType]['careerStats'][heroName]['combat'][statistic] == null) {
+                        document.getElementById(statistic).innerHTML = "N/A";
+                    }
+                    else {
+                        document.getElementById(statistic).innerHTML = dataDownload[gameType]['careerStats'][heroName]['combat'][statistic];
+                    }
+                }
+            }
         });
-        
+
         assistsArray.forEach(function(statistic) {
-            document.getElementById(statistic).innerHTML = data[gameType]['careerStats'][heroName]['assists'][statistic];
+            if (dataDownload[gameType]['careerStats'][heroName] == null) {
+                document.getElementById(statistic).innerHTML = "N/A";
+            }
+            else {
+                if (dataDownload[gameType]['careerStats'][heroName]['assists'] == null) {
+                    document.getElementById(statistic).innerHTML = "N/A";
+                }
+                else {
+                    if (dataDownload[gameType]['careerStats'][heroName]['assists'][statistic] == null) {
+                        document.getElementById(statistic).innerHTML = "N/A";
+                    }
+                    else {
+                        document.getElementById(statistic).innerHTML = dataDownload[gameType]['careerStats'][heroName]['assists'][statistic];
+                    }
+                }
+            }
         });
-        
+
         averageArray.forEach(function(statistic) {
-            document.getElementById(statistic).innerHTML = data[gameType]['careerStats'][heroName]['average'][statistic];
+            if (dataDownload[gameType]['careerStats'][heroName] == null) {
+                document.getElementById(statistic).innerHTML = "N/A";
+            }
+            else {
+                if (dataDownload[gameType]['careerStats'][heroName]['average'] == null) {
+                    document.getElementById(statistic).innerHTML = "N/A";
+                }
+                else {
+                    if (dataDownload[gameType]['careerStats'][heroName]['average'][statistic] == null) {
+                        document.getElementById(statistic).innerHTML = "N/A";
+                    }
+                    else {
+                        document.getElementById(statistic).innerHTML = dataDownload[gameType]['careerStats'][heroName]['average'][statistic];
+                    }
+                }
+            }
         });
-        
+
         awardsArray.forEach(function(statistic) {
-            document.getElementById(statistic).innerHTML = data[gameType]['careerStats'][heroName]['matchAwards'][statistic];
+            if (dataDownload[gameType]['careerStats'][heroName] == null) {
+                document.getElementById(statistic).innerHTML = "N/A";
+            }
+            else {
+                if (dataDownload[gameType]['careerStats'][heroName]['matchAwards'] == null) {
+                    document.getElementById(statistic).innerHTML = "N/A";
+                }
+                else {
+                    if (dataDownload[gameType]['careerStats'][heroName]['matchAwards'][statistic] == null) {
+                        document.getElementById(statistic).innerHTML = "N/A";
+                    }
+                    else {
+                        document.getElementById(statistic).innerHTML = dataDownload[gameType]['careerStats'][heroName]['matchAwards'][statistic];
+                    }
+                }
+            }
         });
-        
+
         gameArray.forEach(function(statistic) {
-            document.getElementById(statistic).innerHTML = data[gameType]['careerStats'][heroName]['game'][statistic];
+            if (dataDownload[gameType]['careerStats'][heroName] == null) {
+                document.getElementById(statistic).innerHTML = "N/A";
+            }
+            else {
+                if (dataDownload[gameType]['careerStats'][heroName]['game'] == null) {
+                    document.getElementById(statistic).innerHTML = "N/A";
+                }
+                else {
+                    if (dataDownload[gameType]['careerStats'][heroName]['game'][statistic] == null) {
+                        document.getElementById(statistic).innerHTML = "N/A";
+                    }
+                    else {
+                        document.getElementById(statistic).innerHTML = dataDownload[gameType]['careerStats'][heroName]['game'][statistic];
+                    }
+                }
+            }
         });
-        
+
         miscellaneousArray.forEach(function(statistic) {
-            document.getElementById(statistic).innerHTML = data[gameType]['careerStats'][heroName]['miscellaneous'][statistic];
+            if (dataDownload[gameType]['careerStats'][heroName] == null) {
+                document.getElementById(statistic).innerHTML = "N/A";
+            }
+            else {
+                if (dataDownload[gameType]['careerStats'][heroName]['miscellaneous'] == null) {
+                    document.getElementById(statistic).innerHTML = "N/A";
+                }
+                else {
+                    if (dataDownload[gameType]['careerStats'][heroName]['miscellaneous'][statistic] == null) {
+                        document.getElementById(statistic).innerHTML = "N/A";
+                    }
+                    else {
+                        document.getElementById(statistic).innerHTML = dataDownload[gameType]['careerStats'][heroName]['miscellaneous'][statistic];
+                    }
+                }
+            }
         });
-    });
+    }
 }
 
 function changeUsernamePlaceholder() {
     if (document.getElementById("searchFormPlatform").value == "pc") {
         document.getElementById("searchFormUsername").placeholder = "Blizzard ID (Username-12345)";
-    } else if (document.getElementById("searchFormPlatform").value == "xbl") {
+    }
+    else if (document.getElementById("searchFormPlatform").value == "xbl") {
         document.getElementById("searchFormUsername").placeholder = "Gamertag";
-    } else if (document.getElementById("searchFormPlatform").value == "psn") {
+    }
+    else if (document.getElementById("searchFormPlatform").value == "psn") {
         document.getElementById("searchFormUsername").placeholder = "PSN ID";
     }
 }
@@ -102,9 +233,11 @@ function changeUsernamePlaceholder() {
 function changeUsernamePlaceholderMobile() {
     if (document.getElementById("searchFormPlatformMobile").value == "pc") {
         document.getElementById("searchFormUsernameMobile").placeholder = "Blizzard ID (Username-12345)";
-    } else if (document.getElementById("searchFormPlatformMobile").value == "xbl") {
+    }
+    else if (document.getElementById("searchFormPlatformMobile").value == "xbl") {
         document.getElementById("searchFormUsernameMobile").placeholder = "Gamertag";
-    } else if (document.getElementById("searchFormPlatformMobile").value == "psn") {
+    }
+    else if (document.getElementById("searchFormPlatformMobile").value == "psn") {
         document.getElementById("searchFormUsernameMobile").placeholder = "PSN ID";
     }
 }
@@ -113,10 +246,12 @@ function changeRegionHidden() {
     if (document.getElementById("searchFormPlatform").value == "pc") {
         var regionDropdown = document.getElementById("searchFormRegion");
         regionDropdown.removeAttribute("hidden");
-    } else if (document.getElementById("searchFormPlatform").value == "xbl") {
+    }
+    else if (document.getElementById("searchFormPlatform").value == "xbl") {
         var regionDropdown = document.getElementById("searchFormRegion");
         regionDropdown.setAttribute("hidden", true);
-    } else if (document.getElementById("searchFormPlatform").value == "psn") {
+    }
+    else if (document.getElementById("searchFormPlatform").value == "psn") {
         var regionDropdown = document.getElementById("searchFormRegion");
         regionDropdown.setAttribute("hidden", true);
     }
@@ -126,10 +261,12 @@ function changeRegionHiddenMobile() {
     if (document.getElementById("searchFormPlatformMobile").value == "pc") {
         var regionDropdownMobile = document.getElementById("searchFormRegionMobile");
         regionDropdownMobile.removeAttribute("hidden");
-    } else if (document.getElementById("searchFormPlatformMobile").value == "xbl") {
+    }
+    else if (document.getElementById("searchFormPlatformMobile").value == "xbl") {
         var regionDropdownMobile = document.getElementById("searchFormRegionMobile");
         regionDropdownMobile.setAttribute("hidden", true);
-    } else if (document.getElementById("searchFormPlatformMobile").value == "psn") {
+    }
+    else if (document.getElementById("searchFormPlatformMobile").value == "psn") {
         var regionDropdownMobile = document.getElementById("searchFormRegionMobile");
         regionDropdownMobile.setAttribute("hidden", true);
     }
